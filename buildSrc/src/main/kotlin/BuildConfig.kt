e7a4df1e3a81fe08eb1703919d75209c36657a93
yairm210 <8366208+yairm210@users.noreply.github.com>
2026-03-09 18:54:07 +0000

    4.19.17-patch2

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.unciv.build
 
 object BuildConfig {
     const val appName = "Unciv"
-    const val appCodeNumber = 1207
-    const val appVersion = "4.19.17-patch1"
+    const val appCodeNumber = 1208
+    const val appVersion = "4.19.17-patch2"
     const val identifier = "com.unciv.app"
 }
